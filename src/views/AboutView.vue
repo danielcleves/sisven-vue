1672fdbb85f2fc401eac6e2d5170b32ff2e500a3
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-05-20 00:29:31 -0500

    ✨ feat: Actualizar vistas About y Home con contenido informativo y estructura mejorada

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container ">
+    <h1 class="mb-3">Acerca de este taller</h1>
+    <p>
+      Quería compartir en esta página mi motivación al realizar este taller de Vue y el enfoque que decidí darle. Como
+      ya llevo
+      más de un año trabajando con Vue en mi empleo, estoy familiarizado con los conceptos básicos del framework. Sin
+      embargo, vi en su taller una oportunidad para reforzar fundamentos, explorar buenas prácticas y, sobre todo,
+      <b>retarme a mí mismo</b>.
+    </p>
+
+    <p>
+      Por eso, aunque el taller estaba planteado como una introducción, decidí añadirle un poco más de "picante" al
+      asunto: implementé el proyecto usando TypeScript. Mi objetivo era no solo repasar Vue, sino también profundizar en
+      cómo TypeScript mejora la escalabilidad y el mantenimiento del código, algo que aplico día a día en mi trabajo.
+    </p>
+
+    <p>
+      Agradezco mucho el espacio para aprender y experimentar, y espero que este taller cumpla con sus expectativas y
+      requerimientos.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container d-flex justify-content-center flex-wrap gap-3">
+
+    <div v-for="card in cards" class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">{{ card.title }}</h5>
+        <p class="card-text">{{ card.text }}</p>
+        <div class="d-flex justify-content-end">
+          <router-link :to="card.link">
+            <a href="#" class="btn btn-primary">Ir</a>
+          </router-link>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+interface Card {
+  title: string;
+  text: string;
+  link: string;
+}
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+const cards: Card[] = [
+  {
+    title: 'Compradores',
+    text: 'Aquí se pueden gestionar los compradores.',
+    link: '/customers',
   },
-});
+  {
+    title: 'Metodos de pago',
+    text: 'Aquí se pueden gestionar los metodos de pago.',
+    link: '/paymodes',
+  },
+  {
+    title: 'Categorías',
+    text: 'Aquí se pueden gestionar las categorías.',
+    link: '/categories',
+  },
+  {
+    title: 'Productos',
+    text: 'Aquí se pueden gestionar los productos.',
+    link: '/products',
+  },
+];
 </script>
